5d55c0cb127c32ce53cb93a5f5aa3c0c8319fb25
Timiha123 <minbomb563@gmail.com>
2026-06-15 17:16:44 +0500

    Delete 12_609-51_RTS directory

--- 12_609-51_RTS/main.js
+++
@@ -1,8 +0,0 @@
-const buttonMenu = document.querySelector('.mobile-nav__header-button')
-const mobileNav = document.querySelector('.mobile-nav')
-
-buttonMenu.addEventListener('click', buttonMenuClicked)
-
-function buttonMenuClicked(){
-    mobileNav.classList.toggle('opened')
-}
